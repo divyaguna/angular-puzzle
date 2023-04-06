e6699e958a157640fdf1e9166f22169be25c1380
Divya Paila <divya.paila1@t-mobile.com>
2023-04-06 12:02:55 +0530

    Added book mark as finished feature

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,45 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should mark book as finished in reading list, I should see "Want to Read" text changed to "Finished" after i marked book as finished in search results, I should be able to delete finished book in reading list and Removing a book will reset the finished status', () => {
+    cy.get('input[type="search"]').type('javascript');
+
+    cy.get('form').submit();
+
+    cy.get('[data-testing="book-item"]')
+      .find('.book--content--info button')
+      .not('[disabled]')
+      .eq(0)
+      .click();
+
+    cy.get('[data-testing="toggle-reading-list"]').click();
+
+    cy.get('[data-testing="reading-list-container"]').should(
+      'contain.text',
+      'My Reading List'
+    );
+
+    cy.get('[data-testing="marked-as-finish-reading-button"]').click();
+
+    cy.get('[data-testing="finished-reading-details"]').contains(
+      'Finished reading'
+    );
+
+    cy.get('[data-testing="book-item"]')
+      .find('.book--content--info button')
+      .eq(0)
+      .contains('Finished');
+
+    cy.get('.reading-list-content')
+      .find('.reading-list-item .mat-button-wrapper')
+      .eq(0)
+      .click();
+
+    cy.get('[data-testing="book-item"]')
+      .find('.book--content--info button')
+      .not('[disabled]')
+      .eq(0)
+      .contains('Want to Read');
+  });
 });
